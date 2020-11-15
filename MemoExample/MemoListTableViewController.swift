8e436346693d4a58aaed7d78fcee0dc41df7a2d8
JiWon Lee <jiwon.swdev@gmail.com>
2020-11-15 16:15:18 +0900

    feat: DataManager 생성, CoreData 연결

--- MemoExample/MemoListTableViewController.swift
+++ MemoExample/MemoListTableViewController.swift
@@ -19,6 +19,9 @@ class MemoListTableViewController: UITableViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        DataManager.shared.fetchMemo()
+        tableView.reloadData()
     }
     
     var token: NSObjectProtocol?
@@ -32,7 +35,7 @@ class MemoListTableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let cell = sender as? UITableViewCell, let indexPath = tableView.indexPath(for: cell) {
             if let vc = segue.destination as? DetailViewController {
-                vc.memo = Memo.dummyMemoList[indexPath.row]
+                vc.memo = DataManager.shared.memoList[indexPath.row]
             }
         }
     }
@@ -49,13 +52,15 @@ class MemoListTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return Memo.dummyMemoList.count
+        return DataManager.shared.memoList.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = Memo.dummyMemoList[indexPath.row].content
-        cell.detailTextLabel?.text = formatter.string(from: Memo.dummyMemoList[indexPath.row].createdDate)
+        
+        let target = DataManager.shared.memoList[indexPath.row]
+        cell.textLabel?.text = target.content
+        cell.detailTextLabel?.text = formatter.string(for: target.createdDate)
         
         return cell
     }
