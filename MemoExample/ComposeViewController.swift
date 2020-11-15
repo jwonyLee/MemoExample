85b55384d9bb4c84e18951d44e5f35c3bcead90f
JiWon Lee <jiwon.swdev@gmail.com>
2020-11-15 15:28:03 +0900

    feat: 메모 저장 기능 구현

--- MemoExample/ComposeViewController.swift
+++ MemoExample/ComposeViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ComposeViewController: UIViewController {
     
+    @IBOutlet weak var memoTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,4 +20,22 @@ class ComposeViewController: UIViewController {
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func save(_ sender: Any) {
+        guard let memo = memoTextView.text, memo.count > 0 else {
+            alert(message: "메모를 입력하세요")
+            return
+        }
+        
+        let newMemo = Memo(content: memo)
+        Memo.dummyMemoList.append(newMemo)
+        
+        NotificationCenter.default.post(name: ComposeViewController.newMemoDidInsert, object: nil)
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ComposeViewController {
+    static let newMemoDidInsert = Notification.Name(rawValue: "newMemoDidInsert")
 }
--- MemoExample/MemoListTableViewController.swift
+++ MemoExample/MemoListTableViewController.swift
@@ -17,14 +17,24 @@ class MemoListTableViewController: UITableViewController {
         return formatter
     }()
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    var token: NSObjectProtocol?
+    
+    deinit {
+        if let token = token {
+            NotificationCenter.default.removeObserver(token)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        token = NotificationCenter.default.addObserver(forName: ComposeViewController.newMemoDidInsert, object: nil, queue: OperationQueue.main) { [weak self] (noti) in
+            self?.tableView.reloadData()
+        }
     }
 
     // MARK: - Table view data source
